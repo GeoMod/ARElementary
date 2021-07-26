a5784a8a7f9ae193dbcb87f80a621a1d27b4b274
Dan O'Leary <olearydaniel@me.com>
2021-07-26 09:43:40 -0500

    Initial Commit

---
+++ ARElementary/ARElementaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARElementaryApp.swift
+//  ARElementary
+//
+//  Created by Daniel O'Leary on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ARElementaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
